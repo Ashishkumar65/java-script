c12a874418d9178fc1b4e480ada2cff1182f4bbb
ashish kumar65 <raushan1ashish@gmail.com>
2021-08-07 23:57:52 +0530

    scope

---
+++ scope.js
@@ -0,0 +1,6 @@
+//lexical scope (static scope)
+//globle scope (defined outside of  all codeblocks)
+//local scope (defined inside a codeblock)
+
+
+//in a scope you can acess variable defined in that scope,or in any parent/ancestor scope
