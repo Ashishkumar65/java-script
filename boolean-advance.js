51dfaa039d7688b422d3d6c37b39d58b8f3ac875
ashish kumar65 <raushan1ashish@gmail.com>
2021-08-07 22:49:22 +0530

    advance boolean using else if

---
+++ boolean-advance.js
@@ -0,0 +1,11 @@
+let temp=45;
+
+if(temp<=31){
+	console.log('its freezing outside');
+}
+else if(temp>=110){
+	console.log('it is hot outside');
+}
+else{
+	console.log('go for it.it is preety nice outside');
+}
